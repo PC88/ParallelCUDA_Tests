301404dfb4e8f10d3cd722b53f3023019a3c251f
PC88 <32823980+PC88@users.noreply.github.com>
2020-07-22 21:51:15 +0100

    code complete

--- dist_v2_cude/Kernel.h
+++ dist_v2_cude/Kernel.h
@@ -1,1 +1,8 @@
 #pragma once
+#ifndef KERNEL_H
+#define KERNEL_H
+
+// kernel wrapper for computing array of distance values
+void distanceArray(float* out, float* in, float ref, int len);
+
+#endif
---
+++ dist_v2_cude/main.cpp
@@ -0,0 +1,30 @@
+#include "Kernel.h"
+#include <stdlib.h>
+#define N 64
+
+float scale(int i, int n)
+{
+	return ((float)i) / (n - 1);
+}
+
+int main()
+{
+	const float ref = 0.5f;
+
+	float* in = (float*)calloc(N, sizeof(float));
+	float* out = (float*)calloc(N, sizeof(float));
+
+	// compute scaled input values
+	for (int i = 0; i < N; ++i)
+	{
+		in[i] = scale(i, N);
+	}
+
+	// compute values for the entire array
+	distanceArray(out, in, ref, N);
+
+	// release resources
+	free(in);
+	free(out);
+	return 0;
+}
